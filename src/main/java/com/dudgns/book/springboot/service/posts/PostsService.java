5a44b681de305e26ec1204922aa0927b40ef02f5
최영훈 <c_yh0327@naver.com>
2022-03-12 19:30:14 +0900

    [feat] findAll boards

--- src/main/java/com/dudgns/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/dudgns/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.dudgns.book.springboot.service.posts;
 
 import com.dudgns.book.springboot.web.domain.posts.Posts;
 import com.dudgns.book.springboot.web.domain.posts.PostsRepository;
+import com.dudgns.book.springboot.web.dto.PostsListResponseDto;
 import com.dudgns.book.springboot.web.dto.PostsResponseDto;
 import com.dudgns.book.springboot.web.dto.PostsSaveRequestDto;
 import com.dudgns.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
